e75f23964ba71943b17bacb4985266766d653083
Laura <LNKEAT@gmail.com>
2023-07-06 17:41:57 -0600

    fix navbar link to home

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,18 +8,18 @@ function Header() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navba r-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="home">Home</Nav.Link>
             <Nav.Link href="About">About</Nav.Link>
-            <Nav.Link href="#services">Services</Nav.Link>
-            <Nav.Link href="#teams">Teams</Nav.Link>
-            <Nav.Link href="#testimonials">Testimonials</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <Nav.Link href="#blog">Blog</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="services">Services</Nav.Link>
+            <Nav.Link href="teams">Teams</Nav.Link>
+            <Nav.Link href="testimonials">Testimonials</Nav.Link>
+            <Nav.Link href="pricing">Pricing</Nav.Link>
+            <Nav.Link href="blog">Blog</Nav.Link>
+            <Nav.Link href="contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
